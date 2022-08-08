eac74d078f1c8ad64fa44d62653551c6ce2c1c83
JovianPlanet <davidpunk@gmail.com>
2022-08-08 13:09:16 -0500

    updated test script

--- fs_test.py
+++ fs_test.py
@@ -1,40 +1,61 @@
 import torch
 from torch.utils.data import DataLoader
+from torchinfo import summary
+
 from datasets.dataloader import FewShot_Dataloader
 from datasets.fewshot_sampler import NShotTaskSampler
 from models.unet import UnetEncoder
 from models.metrics import * 
 from utils.plots import *
 
+#PATH_SUPERVISED = './models/best/fsmulti_weights-jun24.pth'
+PATH_SUPERVISED = './models/fsmul_wts-cross-h8-ep50-10_.pth'
 
-PATH_SUPERVISED = './models/fs_weights.pth'
 TEST_PATH = '/media/davidjm/Disco_Compartido/david/datasets/MRBrainS-All/test'
 
 n_test = 5 # n shots (test)
 k_test = 1 # k way (k classes) (test)
-q_test = 5 # q queries (test)
+q_test = 3 # q queries (test)
 
 episodes_per_epoch = 1
 
+classes = ['GM', 'WM', 'CSF']
+num_classes = len(classes)
+
+num_heads = 2
+
+classes_dic = {'BGR': 0, 'GM': 1, 'WM': 2, 'CSF': 3}
+batch_dice = {'GM': None, 'WM': None, 'CSF': None}
+gen_dice = {'GM': 0.0, 'WM': 0.0, 'CSF': 0.0}
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
+
+torch.cuda.empty_cache()
 
 test_ds = FewShot_Dataloader(
     TEST_PATH,
     'T1.nii', 
     'LabelsForTraining.nii',
     48, 
-    'testing'
+    num_heads
 )
 
 test_mris = DataLoader(
     test_ds, 
-    batch_sampler=NShotTaskSampler(test_ds, episodes_per_epoch, n_test, k_test, q_test),
+    batch_sampler=NShotTaskSampler(test_ds, 
+                                   episodes_per_epoch, 
+                                   n_test, 
+                                   k_test, 
+                                   q_test,
+                                   fixed_tasks=[classes]
+                                  ),
 )
 
-supp = n_test*k_test
+supp = len(classes)*n_test # n_test*k_test
 unet = UnetEncoder(1, depth=5).to(device, dtype=torch.double)
 unet.load_state_dict(torch.load(PATH_SUPERVISED))
+#print(f'{summary(unet.double(), (18, 1, 240, 240))}')
 
 # since we're not training, we don't need to calculate the gradients for our outputs
 unet.eval()
@@ -56,27 +77,20 @@ with torch.no_grad():
         f_s = f[:supp]
         f_q = f[supp:]
 
-        idx_b = y_s==0 # Pertenecen al background
-        idx_1 = y_s==1 # Pertenecen a la clase
-        idx_b = idx_b.unsqueeze(axis=1)
-        idx_1 = idx_1.unsqueeze(axis=1)
+        pv = get_class_prototypes(f_s, y_s)
 
-        f_s_mask = f_s*idx_1
+        dv = get_dists(f_q, pv)
+        mv = torch.argmax(dv, dim=1)
 
-        proto = torch.sum(f_s_mask, dim=(0, 2, 3))
-        sum_ = torch.sum(idx_1)
-        if sum_ == 0:
-            proto = proto*0.0
-        else:
-            proto = proto / sum_
-        proto = proto.view(1, proto.shape[0], 1, 1)
+        for c in classes: #range(mv.shape[0]): # in enumerate(dv):
 
-        dists = cosine_dist(proto, f_q)
+            batch_dice[c] = dice_coeff(torch.where(mv==classes_dic[c], 1, 0), 
+                              torch.where(y_q==classes_dic[c], 1, 0)
+            )
 
-        pval = torch.where(dists>0.8, 1, 0)
-        dice = dice_coeff(pval, y_q)
-        running_dice += dice
-        #print(f'{dice.item()=:.3f}')
-        plot_batch_full(x_q.squeeze(1), y_q, pval>0.8)
+            gen_dice[c] += batch_dice[c].item()
 
-print(f'Val dice = {running_dice/(j + 1):.3f}\n')
+            plot_batch_full(x_q, y_q, mv)
+
+gen_dice = {k: v / (j+1) for k, v in gen_dice.items()}
+print(f'{gen_dice.values()}, {sum(gen_dice.values())/num_classes:.3f}')
